a39aaa1cc67a056f26970bcaa147cc4faa9f0423
hjlarry <hjlarry@163.com>
2019-04-01 15:25:30 +0800

    add capstone to dis .text

--- code/sections.py
+++ code/sections.py
@@ -1,6 +1,7 @@
 import pathlib
 
 from elftools.elf.elffile import ELFFile
+import capstone
 
 
 current = pathlib.Path(__file__).parent
@@ -10,4 +11,12 @@ with open(current / "hello_compiled_linux.elf", "rb") as f:
     print("遍历所有的段:")
     for section in e.iter_sections():
         print(hex(section["sh_addr"]), section.name)
+    print("-" * 20)
+
+    code = e.get_section_by_name(".text")
+    ops = code.data()
+    addr = code["sh_addr"]
+    md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
+    for i in md.disasm(ops, addr):
+        print(f"0x{i.address:x}:\t{i.mnemonic}\t{i.op_str}")
 
