e7dcf51d88bcef9fb1d8a1ac340ca90d29e81278
hjlarry <hjlarry@163.com>
2019-08-27 19:23:45 +0800

    数格子的方式了解汇编

--- assem/frame.c
+++ assem/frame.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int __attribute__((noinline, optimize("-O2"))) add(int x, int y)
+int __attribute__((noinline, optimize("-O0"))) add(long long x, long long y, long long z)
 {
-    return x + y;
+    return x + y + z;
 }
 
 int __attribute__((noinline)) main(int argc, char **argv)
 {
-    int x, y, z;
+    long long x, y, z, a;
     x = 0x11;
     y = 0x22;
-    z = add(x, y);
+    z = 0x33;
+    a = add(x, y, z);
 
-    printf("%d\n", z);
+    printf("%lld\n", a);
 }
