1d2e282292dd636b807860125f4577c96f2359b6
hjlarry <hjlarry@163.com>
2019-09-10 16:38:19 +0800

    go退化赋值示例

--- gocode/main.go
+++ gocode/main.go
@@ -1,5 +1,7 @@
 package main
 
+/*
+一、同名遮蔽示例
 var x = 100
 
 func main() {
@@ -10,4 +12,22 @@ func main() {
 		println(x) // 打印0x8中的x
 	}
 	println(x) // 打印0x18中的x
+}
+*/
+
+/*
+二、退化赋值示例
+反汇编技巧：编译时使用默认的优化模式，但源码中针对某个方法定义禁止内联，这样这个方法就成了一个标志，方便查找和阅读
+*/
+
+//go:noinline
+func test() (int, int) {
+	return 1, 2
+}
+
+func main() {
+	a, x := test()
+	println(a, x)
+	b, x := test()
+	println(b, x)
 }
