39d80df5af20b7d99399c90770687ca6718df3a0
hjlarry <hjlarry@163.com>
2020-08-03 09:40:33 +0800

    add type conversation

--- c/type.c
+++ c/type.c
@@ -14,27 +14,68 @@ short int sh;  这里的int可以省略，所以我们一般写short sh;
 常量的表达式往往是在编译期就计算出来的，例如int days[1+3+31+31];
 字符串常量就是字符数组，在内存上存储字符串时，会存储双引号之间的每一个字符，并在末尾多加一个字符'\0'，实际计算字符串长度时也会不去计算'\0'
 */
-#include <stdio.h>
+// #include <stdio.h>
 
-int strlen(char s[])
+// int strlen(char s[])
+// {
+//     int i;
+//     while (s[i] != '\0')
+//         ++i;
+//     return i;
+// }
+
+// // 还有一种常量是枚举，默认第一个值是0，之后的值不断加1，例如:
+// enum abool
+// {
+//     NO,
+//     YES
+// };
+// enum months
+// {
+//     JAN = 1,
+//     FEB,
+//     MAR,
+//     APR,
+//     MAY
+// };
+
+/*
+类型转化：
+
+运算符两边的数字转换：
+1. 如果任一类型为long double，则将另一个数字转为long double
+2. 否则，若任一为double，则将另一个数字转为double
+3. 否则，若任一为float，则将另一个数字转为float
+4. 否则，将char和short转为int
+5. 然后，若任一为long，则将另一个数字转为long
+*/
+
+// 将一个string类型的数字转化为整数类型 atoi("998") == 998
+int atoi(char s[])
 {
-    int i;
-    while (s[i] != '\0')
-        ++i;
-    return i;
+    int i, n;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+        n = 10 * n + (s[i] - '0');
+    return n;
 }
 
-// 还有一种常量是枚举，默认第一个值是0，之后的值不断加1，例如:
-enum abool
+// char类型的大写转换为小写
+int lower(int c)
 {
-    NO,
-    YES
-};
-enum months
+    if (c >= 'A' && c <= 'Z')
+        return c + 'a' - 'A';
+    else
+        return c;
+}
+
+// 使用 （type）n 进行强制类型转换
+unsigned long int next = 1;
+int rand(void)
 {
-    JAN = 1,
-    FEB,
-    MAR,
-    APR,
-    MAY
-};
+    next = next * 110351245 + 12345;
+    return (unsigned int)(next / 65536) % 32768;
+}
+void srand(unsigned int seed)
+{
+    next = seed;
+}
