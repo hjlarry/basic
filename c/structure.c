2d0c0a5bf240671cefb2a5449d8eb3ae93f5fd15
hjlarry <hjlarry@163.com>
2020-08-10 15:25:57 +0800

    add struct and function

--- c/structure.c
+++ c/structure.c
@@ -24,3 +24,34 @@ struct rect screen;
 //     screen.pt1.y = 300;
 //     printf("%d, %d", pt.x, screen.pt1.y);
 // }
+/*
+二、 struct和方法
+可以传入整个结构体，也可以传入其指针
+*/
+struct point makepoint(int x, int y)
+{
+    struct point temp;
+    temp.x = x;
+    temp.y = y;
+    return temp;
+}
+
+int main()
+{
+    // 指针的写法  (*pp).x 括号必须，有些麻烦，因此有了pp->x的简写。
+    struct point origin, *pp;
+    pp = &origin;
+    origin.x = 100;
+    origin.y = 200;
+    printf("origin is %d, %d \n", origin.x, origin.y);
+    printf("origin is %d, %d \n", (*pp).x, (*pp).y);
+    printf("origin is %d, %d \n", pp->x, pp->y);
+
+    struct rect r, *rp = &r;
+    r.pt1 = *pp;
+    // 以下四种写法等价
+    printf("rect: %d \n", r.pt1.x);
+    printf("rect: %d \n", rp->pt1.x);
+    printf("rect: %d \n", (r.pt1).x);
+    printf("rect: %d \n", (rp->pt1).x);
+}
