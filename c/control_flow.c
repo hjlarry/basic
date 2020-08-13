ca3f9e498ceca9104727f3e260322053f2afe591
hjlarry <hjlarry@163.com>
2020-08-13 15:37:19 +0800

    sort out control flow

--- c/control_flow.c
+++ c/control_flow.c
@@ -1,5 +1,28 @@
 /*
-while和for：
+一、语句与程序块
+在表达式之后加一个分号，它们就变成了语句
+用一对花括号把一组声明和语句括在一起就构成了一个复合语句，也叫做程序块
+*/
+
+/*
+二、 if else
+因为else可以省略，我们要小心一些歧义的写法，例如:
+if (a > b)
+    if (n > 0)
+        z = a
+else
+    z = b
+这样写实际上是把else当做if(n>0)的分支来处理了，编译器并不处理缩进，所以应当用花括号括起来
+*/
+
+/*
+三、 switch
+switch语句中，case只是起一个标号的作用，某个分支执行完后，程序还会进入下一个分支继续执行，除非显式的跳转
+所以经常看到每个分支以一个break结束，最后的default分支的break其实并无必要
+*/
+
+/*
+四、 while和for
 for(expr1;expr2;expr3){
     statement;
 }
@@ -10,6 +33,8 @@ while(expr2){
     expr3;
 }
 其中的expr1、expr2、expr3都是可以省略的
+
+如果语句需要执行简单的初始化和变量递增，使用for语句结构更清晰一些。
 */
 #include <ctype.h>
 #include <stdio.h>
@@ -34,7 +59,7 @@ int main()
 }
 
 /*
-do ... while
+五、 do ... while
 
 do 
     statement
@@ -57,3 +82,9 @@ void itoa(int n, char s[])
     s[i] = '\0';
     reverse(s);
 }
+
+/*
+六、 break continue goto
+continue只用于for、while或do...while，对于switch，某个循环包含了swtich的话，使用continue将直接进入下一个循环
+当有多重循环嵌套时，使用goto是能跳到最外层的，但break不行，但大多数情况下goto会更加难以理解和维护
+*/
