cbd949cdc0b2047948ef105c0412542093b22e76
hjlarry <hjlarry@163.com>
2020-08-17 15:03:07 +0800

    add question 4-4

--- c/practise/chapter4/stack.c
+++ c/practise/chapter4/stack.c
@@ -22,3 +22,37 @@ double pop()
         printf("error: stack is empty!");
     return 0.0;
 }
+
+// 不弹出元素的情况下打印栈顶元素
+void print_stack_top()
+{
+    int i = sp - 1;
+    if (sp > 0)
+        printf("%f", val[i]);
+    else
+        printf("error: stack is empty!");
+}
+
+// 复制栈顶元素
+void copy()
+{
+    double op = pop();
+    push(op);
+    push(op);
+}
+
+// 交换栈顶两个元素的值
+void swap()
+{
+    double op1, op2;
+    op1 = pop();
+    op2 = pop();
+    push(op1);
+    push(op2);
+}
+
+// 清空栈
+void clear()
+{
+    sp = 0;
+}
